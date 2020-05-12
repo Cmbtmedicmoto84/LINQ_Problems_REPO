b0ecc73d6aea1b78137cc0de0f2bc5d314f31b3e
Steven Morimoto <smorimoto2284@gmail.com>
2020-05-12 07:36:24 -0500

    finished problem 1

--- LINQ_Problems/Program.cs
+++ LINQ_Problems/Program.cs
@@ -10,6 +10,22 @@ namespace LINQ_Problems
     {
         static void Main(string[] args)
         {
+
+            List<string> wordLibrary = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            
+
+
+            var queryWordsByLetterCombination = wordLibrary.Where(w => w.Contains("th")).ToList();
+            foreach (var word in queryWordsByLetterCombination)
+            {
+                Console.WriteLine(word);
+                Console.ReadLine();
+            }
+            
+
+
+
+
         }
     }
 }
